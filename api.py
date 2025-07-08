0ea82ce1beb9c704a7a0e284634dfc9f407fadf7
IsmaGold67 <ismaelroberto67@gmail.com>
2025-07-08 16:59:12 -0400

    feat: initial Flask API setup with template folder

---
+++ api.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, render_template
+import json
+
+api = Flask(__name__)
+
+def read_json_db():
+    with open('db.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+@api.route("/vagas", methods=['GET'])
+def listar_vagas():
+    vagas = read_json_db()
+    return render_template('index.html', vagas=vagas)
+
+if __name__ == "__main__":
+    api.run(debug=True) 
